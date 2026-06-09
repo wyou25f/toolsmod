9a7f1f330a2624401f836dcc8d9f1f5e0cdfd2e6
wyou25f <wyou25f@example.com>
2026-06-09 10:35:51 +0300

    1.0.1.2 is out to fabric

---
+++ Releases/26.1.2/toolsplus-release-fabric/src/main/java/com/toolsmod/ToolsMod.java
@@ -0,0 +1,16 @@
+package com.toolsmod;
+
+import com.toolsmod.commands.EnchantCommand;
+import com.toolsmod.listener.HeldEffectListener;
+import net.fabricmc.api.ModInitializer;
+
+public class ToolsMod implements ModInitializer {
+
+    public static final String MOD_ID = "toolsplus";
+
+    @Override
+    public void onInitialize() {
+        EnchantCommand.register();
+        HeldEffectListener.register();
+    }
+}
